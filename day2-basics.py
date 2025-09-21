d96d6d92d6d22a883199a78438561c17b3784d7b
Anmol958 <acetitan00@gmail.com>
2025-09-21 09:44:44 +0530

    Day 2: Variables and Input

---
+++ day2-basics.py
@@ -0,0 +1,18 @@
+name = "abc"
+age = 19
+height = 160.5
+fav_color = "Red"
+hobby = "coding" 
+print("my name is",name)
+print("my age is",age)
+print("my height is",height)
+print("my favorite color is",fav_color)
+print("my hobby is",hobby)
+
+u_name=input("enter your name: ")
+u_age=int(input("enter your age : "))
+u_fav_color=input("enter your favorite color : ")
+u_hobby=input("enter your hobby :")
+
+print("hello", u_name, "! you are", u_age, "years old.")
+print("Your favorite color is", u_fav_color, "and your hobby is", u_hobby)
